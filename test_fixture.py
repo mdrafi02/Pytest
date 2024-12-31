43cd906585de9f9fe8567ff069e29849d652c6fd
Mahamadrafi S <mahammedrafi02@gmail.com>
2024-12-31 10:53:01 +0530

    "Requesting Fixture" a quick example

--- test_fixture.py
+++ test_fixture.py
@@ -1,19 +1,30 @@
-import pytest
+import pytest 
 
+class Fruit:
+    def __init__(self,name):
+        self.name = name
+        self.cubed = False
+
+    def cube(self):
+        self.cubed = True
+
+class FruitSalad:
+    def __init__(self, *fruit_bowl):
+        self.fruit = fruit_bowl
+        self._cube_fruit()
+
+    def _cube_fruit(self):
+        for fruit in self.fruit:
+            fruit.cube()
+
+#Arrange
 @pytest.fixture
-def numbers():
-    a = 10
-    b = 20
-    c = 30
-    return [a,b,c]
+def fruit_bowl():
+    return [Fruit("apple"), Fruit("Banana")]
 
-@pytest.mark.xfail
-def test_method1(numbers):
-    assert numbers[0] == 20
+def test_fruit_salad(fruit_bowl):
+    #Act
+    fruit_salad = FruitSalad(*fruit_bowl)
 
-@pytest.mark.skip
-def test_method2(numbers):
-    assert numbers[1] == 20
-
-def test_method3(numbers):
-    assert numbers[2] == 30
+    #Assert
+    assert all(fruit.cubed for fruit in fruit_salad.fruit)
