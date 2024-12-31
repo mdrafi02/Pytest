0ba9875bea7e1b94f0a9c81fb9975aebb5e4b1a4
Mahamadrafi S <mahammedrafi02@gmail.com>
2024-12-31 13:12:54 +0530

    Scope : Sharing fixtures across classes , modules, packages or session

--- conftest.py
+++ conftest.py
@@ -1,4 +1,6 @@
 from test_foocompare import Foo
+import smtplib
+import pytest
 
 def pytest_assertrepr_compare(op, left, right):
     if isinstance(left, Foo) and isinstance(right, Foo) and op == "==":
@@ -7,3 +9,6 @@ def pytest_assertrepr_compare(op, left, right):
             f"  vals: {left.val} != {right.val}",
         ]
     
+@pytest.fixture(scope="module")
+def smtp_connection():
+    return smtplib.SMTP("smtp.gmail.com", 587, timeout=5)
